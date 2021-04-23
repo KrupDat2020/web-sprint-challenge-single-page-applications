45ad5299ad9b5068773989ecd007cc6c3a974fdb
KrupDat2020 <mark.krupinsky@gmail.com>
2021-04-23 10:46:27 -0500

    pizza form finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PizzaForm from './PizzaForm';
 
 const App = () => {
   return (
---
+++ src/PizzaForm.js
@@ -0,0 +1,106 @@
+import React from 'react';
+
+export default function PizzaForm(props) {
+    const { values, submit, change, disabled, errors } = props;
+
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+        submit();
+    };
+
+    const onChange = (evt) => {
+        const { name, value, checked, type } = evt.target; 
+        const valueToUse = type === "checkbox" ? checked : value;
+        change(name, valueToUse);
+    };
+
+    return (
+        <form className="form-container" onSubmit={onSubmit}>
+            <div className="form-group submit">
+                <h2>Add some Toppings</h2>
+                <button disabled={disabled}>Submit</button>
+
+                <div className="errors">
+                    <div>{errors.username}</div>
+                    <div>{errors.size}</div>
+                    <div>{errors.special}</div>
+                    <div>{errors.toppings}</div>
+                </div>
+            </div>
+
+            <div className="form-group inputs">
+                <h4>Please Enter Your Information</h4>
+
+                <label>
+                    Username:
+                    <input
+                       value={values.username}
+                       onChange={onChange}
+                       name="username"
+                       type="text"
+                       placeholder="please enter your name"
+                       />
+                </label>
+
+                <label>
+                   Size:
+                 <input
+                  value={values.size}
+                  onChange={onChange}
+                 name="size"
+                 type="text"
+                 placeholder="please enter size requested"
+                 />
+                </label>
+
+                <label>
+                   Special:
+                <input
+                  value={values.special}
+                  onChange={onChange}
+                  name="special"
+                  type="text"
+                  placeholder="please enter any special requests"
+                  />
+                </label>
+
+                <label>
+                    Pepperoni
+                    <input
+                    type="checkbox"
+                    name="pepperoni"
+                    checked={values.pepperoni}
+                    onChange={onChange}
+                    />
+                </label>
+                <label>
+                    Sausage
+                    <input
+                    type="checkbox"
+                    name="sausage"
+                    checked={values.sausage}
+                    onChange={onChange}
+                    />
+                 </label>
+                 <label>
+                     Mushroom
+                     <input
+                     type="checkbox"
+                     name="mushroom"
+                     checked={values.mushroom}
+                     onChange={onChange}
+                     />
+                 </label>
+                 <label>
+                     Peppers
+                     <input
+                     type="checkbox"
+                     name="peppers"
+                     checked={values.peppers}
+                     onChange={onChange}
+                     />
+                 </label>
+            </div>
+        </form>
+    );
+}
